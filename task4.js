9e264854bf6048073d5f630851935ca940d11e8d
sujauddoula352 <sujauddoulasohel352@gmail.com>
2025-10-17 00:28:10 +0600

    feat: Add function to count zeros in a number string

---
+++ task4.js
@@ -0,0 +1,21 @@
+function count_zero(stri) {
+    const strings = String(stri)
+    let count = 0;
+    for (let char of strings) {
+        if (char === '0') {
+            count++
+        }
+    }
+    return count
+
+}
+console.log(count_zero(101010777000));
+
+
+function num_check(numbers){
+const num1 = 0
+for(let num of numbers){
+    
+}
+
+}
